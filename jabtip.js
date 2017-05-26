8fe651964be355af300873b2741f6707c4e94e27
Jonatas Emidio <jonatasemidio@gmail.com>
2017-05-26 14:31:02 -0300

    Add Obj Tip
    This is an Obj to use instead of JSON file to perf

--- jabtip.js
+++ jabtip.js
@@ -1,4 +1,17 @@
-function read(file, callback) {
+function Tip(id, name, img, link, template){
+
+  this.id = id;
+  this.name = name;
+  this.img = img;
+  this.link = link;
+  this.template = template;
+
+  this.run = function(){alert("RUN!! " + this.name);};
+}
+
+
+function readFile(file, callback) {
+  alert("readFile");
     var rawFile = new XMLHttpRequest();
     rawFile.overrideMimeType("application/json");
     rawFile.open("GET", file, true);
@@ -9,3 +22,17 @@ function read(file, callback) {
     }
     rawFile.send(null);
 }
+
+
+
+function readClass(obj, callback) {
+  alert("readClass");
+
+  var rawFile = new XMLHttpRequest();
+  rawFile.onreadystatechange = function() {
+      if (rawFile.readyState === 4 && rawFile.status == "200") {
+          callback(obj);
+      }
+  }
+}
+
